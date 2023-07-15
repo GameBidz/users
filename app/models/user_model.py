12929c429e6c03790c03105b013826128accb9de
jusruizsa <sruizs.jr@gmail.com>
2023-07-14 20:46:18 -0500

    remove id User attr and change to_dict function

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -1,14 +1,8 @@
 class User:
-    def __init__(self, id, username, email, password) -> None:
-        self.id = id
+    def __init__(self, username, email, password) -> None:
         self.username = username
         self.email = email
         self.password = password
 
-    def to_dict(self):
-        return {
-            'id': self.id,
-            'username': self.username,
-            'email': self.email,
-            'password': self.password
-        }
+    def to_dict(self) -> dict:
+        return dict(username=self.username, email=self.email, password=self.password)
